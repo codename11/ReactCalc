c501b9c7a2bd05cc20e67b2f572f559d737707e1
Veljko <veljkos82@gmail.com>
2019-08-17 22:02:23 +0200

    Updates

---
+++ precache-manifest.b555e98dbc2984a8716a8a3ff2e9cd19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f48154be828ea3d564ab1452d7c8d4",
+    "url": "/ReactCalc/index.html"
+  },
+  {
+    "revision": "84a819f88ecae54704c8",
+    "url": "/ReactCalc/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "14941359ec98e923791a",
+    "url": "/ReactCalc/static/css/main.856fa91b.chunk.css"
+  },
+  {
+    "revision": "84a819f88ecae54704c8",
+    "url": "/ReactCalc/static/js/2.87146427.chunk.js"
+  },
+  {
+    "revision": "14941359ec98e923791a",
+    "url": "/ReactCalc/static/js/main.c7ee1ad4.chunk.js"
+  },
+  {
+    "revision": "1a5236b573d58d4e00a9",
+    "url": "/ReactCalc/static/js/runtime~main.d49276e6.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/ReactCalc/static/media/digital-7.1e670d88.ttf"
+  }
+]);
