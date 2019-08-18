c58534e6e8b8207ef1b0dfb51532e9524283d10c
Veljko <veljkos82@gmail.com>
2019-08-19 01:52:03 +0200

    Added second num

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ class Calculator  extends React.Component {
 		super(props);
 		this.state = {
             firstNum: false,
-            firstNumber: null,
             secondNum: false,
-            secondNumber: null,
             operator: false,
             typeOfOperator: null,
-            display: ""
+            display: 0,
+            equalSign: false,
+            result: null
 		};
 		this.handleClick = this.handleClick.bind(this);
         this.firstNum = this.firstNum.bind(this);
         this.operator = this.operator.bind(this);
+        this.secondNum = this.secondNum.bind(this);
+        this.equal = this.equal.bind(this);
+        this.calculate = this.calculate.bind(this);
     }
 
     handleClick(e){
@@ -30,22 +33,19 @@ class Calculator  extends React.Component {
         if(this.firstNum(pressed) && this.state.firstNum === false){
             arr.push(this.firstNum(pressed));
             this.setState({
-                display: this.state.display+this.firstNum(pressed),
+                display: this.state.display===0 ? this.firstNum(pressed) : this.state.display+this.firstNum(pressed),
             });
         }
 
         if(this.operator(pressed) && this.state.secondNum === false && this.state.operator === false){
             /*Checks if operator exists, if it not exists, puts one.*/
-            
+            arr.push(this.operator(pressed));
             this.setState({
                 typeOfOperator: this.operator(pressed),
                 operator: true,
                 firstNum: true,
-                firstNumber: arr.join(""),
                 display: this.state.display+this.operator(pressed),
             });
-
-            arr.push(this.operator(pressed));
 
         }
 
@@ -63,6 +63,21 @@ class Calculator  extends React.Component {
 
         }
 
+        if(this.secondNum(pressed) && this.state.firstNum === true && this.state.secondNum === false && this.state.equalSign === false){
+
+            arr.push(this.secondNum(pressed));
+            this.setState({
+                display: this.state.display===0 ? this.secondNum(pressed) : this.state.display+this.secondNum(pressed),
+            });
+
+        }
+
+        if(this.equal(pressed) && this.state.firstNum === true && this.state.operator === true && this.state.secondNum === true){
+            
+            arr.push(this.equal(pressed));
+            
+        }
+        
 	}
 
     firstNum(num){
@@ -125,16 +140,78 @@ class Calculator  extends React.Component {
 
     }
 
+    secondNum(num){
+
+        switch(num) {
+
+            case "0":
+              return num;
+
+            case "1":
+                return num;
+
+            case "2":
+                return num;
+
+            case "3":
+                return num;
+
+            case "4":
+                return num;
+
+            case "5":
+                return num;
+
+            case "6":
+                return num;
+
+            case "7":
+                return num;
+
+            case "8":
+                return num;
+
+            case "9":
+                return num;
+
+            default:
+              console.log("Not an number!");
+              return null;
+          
+        }
+
+    }
+
+    equal(operator){
+
+        switch(operator) {
+            
+            case "=":
+                this.setState({
+                    equalSign: true,
+                    secondNum: true,
+                    result: this.calculate()
+                });
+                
+                return operator;
+
+            default:
+                console.log("Not an equal sign!");
+                return null;
+          }
+          
+    }
+
+    calculate(){
+        return eval(this.state.display);  
+    }
 
     render(){
-    
-
-        //let nums = this.state.formula && (this.state.formula[this.state.formula.length-1] === "+" || "-" || "*" || "/")
         console.log(this.state);
-        console.log(arr);
+        
         return(
             <div id="calculator" className="grid-container cent">
-                <div className="item1x1">{this.state.display}</div>
+                <div className="item1x1">{this.state.result ? this.state.result : this.state.display}</div>
                 <div className="item1x2">{this.state.firstNumber}</div>
                 <button className="drum-pad metal linear item2" value="AC" onClick={this.handleClick}>AC</button>
                 <button className="drum-pad metal linear" value="/" onClick={this.handleClick}>/</button>
